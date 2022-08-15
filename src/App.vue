89ea698e0b90bd5fb7cc43caedd0657009e59953
HUAMINO <1906448030@qq.com>
2022-08-15 20:15:31 +0800

    add header

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+   <!-- <img src="./assets/logo.png">-->
+<!--<router-view/>显示的是当前路由地址所对应的内容 即用户输入什么地址，就显示这个地址的内容。-->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<!--路由就是根据网址的不同，返回不同的内容给用户。-->
+<style>
+</style>
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="header">
+        <div class="header-left">
+            <div class="iconfont back-icon">&#xe685;</div>
+        </div>
+        <div class="header-input">
+            <span class="iconfont">&#xe600;</span>
+            输入城市/景点/游玩主题</div>
+        <div class="header-right">
+            城市
+            <span class="iconfont">&#xe601;</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl'
+    .header
+        display: flex
+        line-height: .86rem
+        color: #fff
+        background-color: $bgColor
+        .header-left
+            width: .64rem
+            float: left
+            .back-icon
+                text-align: center
+                font-size: .4rem
+        .header-right
+            float: right
+            width: 1.24rem
+            text-align: center
+        .header-input
+            flex: 1
+            background: #fff
+            margin-top: .12rem
+            height: .64rem
+            line-height: .64rem
+            border-radius: .1rem
+            margin-left: .2rem
+            padding-left: .2rem
+            color: grey
+</style>
